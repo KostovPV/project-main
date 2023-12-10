723fd1ff6dcf3690a6e08232268d74889c56984a
KostovPV <106186518+KostovPV@users.noreply.github.com>
2023-12-10 22:23:22 +0200

    FindUs edited

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./About.css";
+
 function About() {
   const [showMore, setShowMore] = useState(false);
 
@@ -22,13 +23,15 @@ function About() {
                   Welcome To Our Website
                 </span>
                 <h1 className="mb-3 font-weight-bold text-teal">Contact Us</h1>
-                <p>
-                  {/* <NavLink to="/" className="text-white">
-                    Home
-                  </NavLink> */}
-                  <span className="mx-3"><strong>Telephone: 0897011667</strong></span><br/>
-                  <span className="mx-3"><strong>Email: kidscenter.burgas@gmail.com</strong></span>
-                </p>
+                <div>
+                  <span className="mx-3">
+                    <strong>Telephone: 0897011667</strong>
+                  </span>
+                  <br />
+                  <span className="mx-3">
+                    <strong>Email: kidscenter.burgas@gmail.com</strong>
+                  </span>
+                </div>
               </div>
             </div>
           </div>
@@ -43,37 +46,47 @@ function About() {
             <div className="col-md-5 ml-auto pl-md-5">
               <span className="text-cursive h5 text-red">About Us</span>
               <h3 className="text-black">Bring Fun Life To Your Kids</h3>
-              <p>
-                <span>
-                  The Kid's center core idea is to create a pleasant atmosphere which will help the children to have a quality time together.
-                </span>
-                <span>
-                  We will provide the best serivice for you kid's party.
-                </span>
-              </p>
-              <p className="mt-5">
-                {/* <a href="#" className="btn btn-warning py-4 btn-custom-1">
-                  More About Us
-                </a> */}
-                <div style={{ display: showMore ? "block" : "none" }}>
-                  <p>
-                  Since April 2023 we have been trying to create the best memories for you and your children. The kids smiles and happy look is what we aim at. You can find is at Izgrev bl 4, or simply follow the link:
-                    <span >     <NavLink to="/findus" >
-                      Find us
-                    </NavLink></span>
-
-                  </p>
-
-                  <a onClick={toggleShowMore} className="btn btn-primary btn-custom-1 mt-4">
-                    Show less
-                  </a>
+              <div>
+                <div>
+                  <span>
+                    The Kid's center core idea is to create a pleasant atmosphere
+                    which will help the children to have a quality time together.
+                  </span>
+                  <span>
+                    We will provide the best service for your kid's party.
+                  </span>
                 </div>
-                {!showMore && (
-                  <a onClick={toggleShowMore} className="btn btn-primary btn-custom-1 mt-4">
-                    More About Us
-                  </a>
-                )}
-              </p>
+                <div className="mt-5">
+                  <div>
+                    <a
+                      onClick={toggleShowMore}
+                      className="btn btn-primary btn-custom-1 mt-4"
+                    >
+                      {showMore ? "Show less" : "More About Us"}
+                    </a>
+                  </div>
+                  {showMore && (
+                    <div>
+                      <p>
+                        Since April 2023, we have been trying to create the best
+                        memories for you and your children. The kids' smiles and
+                        happy look are what we aim at. You can find us at Izgrev bl
+                        4, or simply follow the link:
+                        <span>
+                          {" "}
+                          <NavLink to="/findus">Find us</NavLink>
+                        </span>
+                      </p>
+                      <a
+                        onClick={toggleShowMore}
+                        className="btn btn-primary btn-custom-1 mt-4"
+                      >
+                        Show less
+                      </a>
+                    </div>
+                  )}
+                </div>
+              </div>
             </div>
           </div>
         </div>
--- src/pages/FindUs/FIndUs.jsx
+++ src/pages/FindUs/FIndUs.jsx
@@ -1,9 +1,123 @@
 
 
 
-import React, { useEffect } from 'react';
+// import React, { useEffect } from 'react';
+
+// const FindUs = () => {
+//   const centerLocation = { lat: 42.525899, lng: 27.454538 };
+//   let map;
+//   let directionsService;
+//   let directionsRenderer;
+
+//   useEffect(() => {
+//     const loadScript = () => {
+//       const script = document.createElement('script');
+//   // script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAeze8SmEQ1zLTS43pSmM-ZnHViQom20Eg&libraries=places`;
+//   script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAX6SEchN9tHFooLh3-FH9FP4vOpOGQVHw&libraries=places`;
+// // 'AIzaSyAX6SEchN9tHFooLh3-FH9FP4vOpOGQVHw'
+//       script.async = true;
+//       document.head.appendChild(script);
+
+//       // Define the callback function
+//       window.initMap = initializeMap;
+
+//       script.onload = () => {
+//         initializeMap();
+//       };
+
+//       script.onerror = () => {
+//         console.error('Error loading Google Maps API.');
+//       };
+//     };
+
+//     // Check if the Google Maps API is already loaded
+//     if (window.google && window.google.maps) {
+//       initializeMap();
+//     } else {
+//       loadScript();
+//     }
+
+//     return () => {
+//       // Remove the callback function from the global scope
+//       delete window.initMap;
+//     };
+//   }, []); 
+
+//   const initializeMap = () => {
+//     map = new window.google.maps.Map(document.getElementById('map'), {
+//       center: { lat: centerLocation.lat, lng: centerLocation.lng },
+//       zoom: 15,
+//     });
+
+//     directionsService = new window.google.maps.DirectionsService();
+//     directionsRenderer = new window.google.maps.DirectionsRenderer({
+//       map: map,
+//     });
+
+//     // Get the user's current location
+//     navigator.geolocation.getCurrentPosition(
+//       (position) => {
+//         const userLocation = {
+//           lat: position.coords.latitude,
+//           lng: position.coords.longitude,
+//         };
+    
+//         // Display user marker on the map
+//         new window.google.maps.Marker({
+//           position: userLocation,
+//           map: map,
+//           title: 'Your Location',
+//         });
+    
+//         // Calculate and display directions
+//         calculateAndDisplayRoute(userLocation);
+//       },
+//       (error) => {
+//         console.error('Error getting user location:', error);
+    
+//         // Default to a fallback location if user's location cannot be obtained
+//         const fallbackLocation = { lat: 42.525899, lng: 27.454538 };
+//         calculateAndDisplayRoute(fallbackLocation);
+//       }
+//     );
+//   };
+
+//   const calculateAndDisplayRoute = (userLocation) => {
+//     const request = {
+//       origin: new window.google.maps.LatLng(userLocation.lat, userLocation.lng),
+//       destination: new window.google.maps.LatLng(centerLocation.lat, centerLocation.lng),
+//       travelMode: 'DRIVING',
+//     };
+  
+//     directionsService.route(request, (result, status) => {
+//       if (status === 'OK') {
+//         directionsRenderer.setDirections(result);
+//         // You can also access distance and duration from 'result' object
+//         console.log('Distance:', result.routes[0].legs[0].distance.text);
+//         console.log('Duration:', result.routes[0].legs[0].duration.text);
+//       } else {
+//         console.error('Error calculating route:', status);
+//       }
+//     });
+//   };
+  
+
+//   return(
+//     <div className='findus-container'>
+//      <div id="map" style={{ height: '900px', width: '100%' }}>
+
+//   </div>
+//   </div>)
+// };
+
+// export default FindUs;
+
+
+import React, { useEffect, useState } from 'react';
 
 const FindUs = () => {
+  const [loading, setLoading] = useState(true);
+
   const centerLocation = { lat: 42.525899, lng: 27.454538 };
   let map;
   let directionsService;
@@ -12,25 +126,23 @@ const FindUs = () => {
   useEffect(() => {
     const loadScript = () => {
       const script = document.createElement('script');
-  // script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAeze8SmEQ1zLTS43pSmM-ZnHViQom20Eg&libraries=places`;
-  script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAX6SEchN9tHFooLh3-FH9FP4vOpOGQVHw&libraries=places`;
-// 'AIzaSyAX6SEchN9tHFooLh3-FH9FP4vOpOGQVHw'
+      script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAX6SEchN9tHFooLh3-FH9FP4vOpOGQVHw&libraries=places`;
       script.async = true;
       document.head.appendChild(script);
 
-      // Define the callback function
       window.initMap = initializeMap;
 
       script.onload = () => {
+        setLoading(false);
         initializeMap();
       };
 
       script.onerror = () => {
+        setLoading(false);
         console.error('Error loading Google Maps API.');
       };
     };
 
-    // Check if the Google Maps API is already loaded
     if (window.google && window.google.maps) {
       initializeMap();
     } else {
@@ -38,10 +150,9 @@ const FindUs = () => {
     }
 
     return () => {
-      // Remove the callback function from the global scope
       delete window.initMap;
     };
-  }, []); 
+  }, []);
 
   const initializeMap = () => {
     map = new window.google.maps.Map(document.getElementById('map'), {
@@ -54,28 +165,24 @@ const FindUs = () => {
       map: map,
     });
 
-    // Get the user's current location
     navigator.geolocation.getCurrentPosition(
       (position) => {
         const userLocation = {
           lat: position.coords.latitude,
           lng: position.coords.longitude,
         };
-    
-        // Display user marker on the map
+
         new window.google.maps.Marker({
           position: userLocation,
           map: map,
           title: 'Your Location',
         });
-    
-        // Calculate and display directions
+
         calculateAndDisplayRoute(userLocation);
       },
       (error) => {
         console.error('Error getting user location:', error);
-    
-        // Default to a fallback location if user's location cannot be obtained
+
         const fallbackLocation = { lat: 42.525899, lng: 27.454538 };
         calculateAndDisplayRoute(fallbackLocation);
       }
@@ -88,11 +195,10 @@ const FindUs = () => {
       destination: new window.google.maps.LatLng(centerLocation.lat, centerLocation.lng),
       travelMode: 'DRIVING',
     };
-  
+
     directionsService.route(request, (result, status) => {
       if (status === 'OK') {
         directionsRenderer.setDirections(result);
-        // You can also access distance and duration from 'result' object
         console.log('Distance:', result.routes[0].legs[0].distance.text);
         console.log('Duration:', result.routes[0].legs[0].duration.text);
       } else {
@@ -100,14 +206,13 @@ const FindUs = () => {
       }
     });
   };
-  
 
-  return(
+  return (
     <div className='findus-container'>
-     <div id="map" style={{ height: '900px', width: '100%' }}>
-
-  </div>
-  </div>)
+      {loading && <p>Loading...</p>}
+      <div id="map" style={{ height: '100vh', width: '100%' }} />
+    </div>
+  );
 };
 
 export default FindUs;
